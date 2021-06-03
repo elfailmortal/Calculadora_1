c52965d7742bb52a80b5c317eac31f46692f8fe0
El Failmortal <73493735+elfailmortal@users.noreply.github.com>
2021-06-03 14:35:57 -0500

    Create Calculadora Python.py

---
+++ Calculadora Python.py
@@ -0,0 +1,9 @@
+import math
+import cmath
+import 
+#Informacion del usuario
+nombre = input('Bienvenido, ¿cuál es tu nombre?: ')
+print(str("Así que tu nombre es {0}, que fantastico nombre tienes").format(nombre))
+respuesta_1 = input("¿Quieres realizar una operación?: ")
+if resouest
+  
